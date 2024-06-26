b90b05f738ad80164503c17e2ee62c3318e5ff7f
Benja Martinez <be.martinezg@duocucl>
2024-06-26 15:50:37 -0400

    Prueba numero 5

--- Alumno.py
+++ Alumno.py
@@ -1,8 +1,22 @@
-def menu(menu):
+def menu():
     print("1.- Ingresar Alumno: ")
     print("2.- Eliminar Alumno: ")
     print("3.- Ver listado de Alumno")
     print("4.- Salir y Guardar")
-    print("Ingrese Opción.")
-    
+
+
 menu()
+opcion = 0
+while True:
+    opcion = int(input("Ingrese Opción: "))
+    if opcion == 1:
+        print("Prueba opcion Ingreso")
+    elif opcion == 2:
+        print("Prueba Eliminar Alumno")
+    elif opcion == 3:
+        print("Prueba Listado de Alumno")
+    elif opcion == 4:
+        print("Saliendo del programa...")
+        break
+    else:
+        print("Opcion errónea.")
