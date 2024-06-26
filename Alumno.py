7861d7ef958d17b580a097252a081e6416dd979e
Benja Martinez <be.martinezg@duocucl>
2024-06-26 16:10:33 -0400

    Prueba Numero 6

--- Alumno.py
+++ Alumno.py
@@ -4,19 +4,26 @@ def menu():
     print("3.- Ver listado de Alumno")
     print("4.- Salir y Guardar")
 
-
 menu()
+
+alumnos = {
+    "Nombre": "Pepe",
+    "Rut": "12345678",
+    "Nota": [7.0,4.0,6.5]
+}
 opcion = 0
 while True:
     opcion = int(input("Ingrese Opción: "))
     if opcion == 1:
-        print("Prueba opcion Ingreso")
+        print("Ingresando Alumno: ")   
     elif opcion == 2:
         print("Prueba Eliminar Alumno")
     elif opcion == 3:
-        print("Prueba Listado de Alumno")
+        print("Verlistado de Alumno")
+        print(alumnos)
     elif opcion == 4:
         print("Saliendo del programa...")
         break
     else:
         print("Opcion errónea.")
+
